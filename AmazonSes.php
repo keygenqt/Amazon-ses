135c2a12565da4edb2b8e472da65e199f414aade
KeyGenQt <keygenqt@gmail.com>
2016-02-27 20:15:41 +0300

    first commit

---
+++ AmazonSes.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace keygenqt\amazonSes;
+
+use \yii\base\Exception;
+use yii\base\Widget;
+
+class AmazonSes extends Widget
+{
+    const AWS_US_EAST_1 = 'email.us-east-1.amazonaws.com';
+    const AWS_US_WEST_2 = 'email.us-west-2.amazonaws.com';
+    const AWS_EU_WEST1 = 'email.eu-west-1.amazonaws.com';
+
+    public $access;
+    public $secret;
+    public $email;
+    public $host = self::AWS_EU_WEST1;
+
+    private $ses;
+
+    public function init()
+    {
+        if (!$this->access || !$this->secret) {
+            throw new Exception("Access and secret required!");
+        }
+
+        $this->ses = new \SimpleEmailService($this->access, $this->secret, $this->host);
+
+        parent::init();
+    }
+
+    public function send(array $emails, $subject, $body)
+    {
+        $m = new \SimpleEmailServiceMessage();
+        $m->setFrom($this->email);
+        $m->addTo($emails);
+
+        $m->setSubject($subject);
+        $m->setMessageFromString($body);
+
+        return $this->ses->sendEmail($m);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+yii2-amazon-ses
+===================
+
+Widget from send email amazon ses.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either add
+
+```
+"require": {
+    "keygenqt/yii2-amazon-ses": "*"
+}
+```
+
+of your `composer.json` file.
+
+## Latest Release
+
+The latest version of the module is v0.5.0 `BETA`.
+
+## Usage
+
+
+Config:
+
+```php
+'amazonSes' => [
+    'class' => 'keygenqt\amazonSes\AmazonSes',
+    'email' => '...',
+    'access' => '...',
+    'secret' => '...',
+    'host' => '...', (optional - default email.eu-west-1.amazonaws.com)
+]
+```
+
+Upload:
+
+```php
+$result = Yii::$app->amazonSes->send(['email@email.com'], 'Subject', 'Body');
+```
+
+## License
+
+**yii2-amazon-ses** is released under the BSD 3-Clause License. See the bundled `LICENSE.md` for details.
+
+
